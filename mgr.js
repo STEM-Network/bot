daf5c586c3148d65c6abd05c302cf5238152f630
Azurethi <Github@Azurethi.me>
2020-06-25 17:48:16 +0100

    Added load waiting to mgr

--- mgr.js
+++ mgr.js
@@ -3,13 +3,15 @@ module.exports=(cli, db, log, options={regex:/^\/([a-zA-Z0-9_.]*)( |$)/})=>{
         list:[],
         handlers:{}
     }
+    var waitingFor = {};
     cli.on('message', msg=>{
         var cmd=options.regex.exec(msg.content);
         //TODO extract cmd args
         if(cmd != null && cmd[1]){
             var tlc = cmd[1].toLocaleLowerCase()
+            var args = msg.content.split(' ');  //TODO better argument parsing (eg: quoted strings)
             if(registeredCMDS.list.includes(tlc)){
-                registeredCMDS.handlers[tlc](false,msg,[]);
+                registeredCMDS.handlers[tlc](false,msg,args);
             }
         }
     })
@@ -20,13 +22,17 @@ module.exports=(cli, db, log, options={regex:/^\/([a-zA-Z0-9_.]*)( |$)/})=>{
                     if(err) handler(err);
                     db.get('users',(err,users)=>{
                         if(err){
-                            handler(err)
+                            handler(err,msg,args)
                         } else {
                             users.get(msg.user.id, (err,userdata)=>{
                                 if(err){
-                                    handler(err)  //TODO create userdata if required
+                                    if(options.createNew && err == "Does not exist in users!"){
+                                        handler(false,msg,args,users.create(msg.user.id, msg.user.tag),users);
+                                    } else {
+                                        handler(err,msg,args,null,null)
+                                    }
                                 } else {
-                                    handler(false, msg, args, userdata)
+                                    handler(false, msg, args, userdata,users)
                                 }
                             })
                         }
@@ -40,17 +46,55 @@ module.exports=(cli, db, log, options={regex:/^\/([a-zA-Z0-9_.]*)( |$)/})=>{
         getUserdata:(id, next)=>{
             db.get('users',(err,users)=>{
                 if(err){
-                    next('err')
+                    next(err)
                 } else {
                     users.get(id, (err,userdata)=>{
                         if(err){
-                            next('err')  //TODO create userdata if required
+                            next(err)
                         } else {
                             next(false, userdata);
                         }
                     })
                 }
             })
+        },
+        waitFor:(modDesc)=>{
+           if(waitingFor[modDesc.name]){
+               log(1, `Module name conflict: \n${JSON.stringify(modDesc)} With ${JSON.stringify(waitingFor[modDesc.name].desc)}`);
+           } else {
+                waitingFor[modDesc.name] = {
+                    desc: modDesc,
+                    ready:false
+                }
+           }
+        },
+        readyUp:(modDesc)=>{
+            waitingFor[modDesc.name].ready=true;
+            log(3, `${modDesc.name} module ready`);
+        },
+        onReady:(func)=>{
+            var waited = 0;
+            var waitingInterval = setInterval(()=>{
+                var allReady = true;
+                var mods = Object.keys(waitingFor);
+                for(var i=0; i<mods.length; i++){
+                    if(!waitingFor[mods[i]].ready) allReady=false;
+                }
+                if(allReady){
+                    clearInterval(waitingInterval);
+                    log(3, "All modules ready");
+                    func();
+                } else if(waited>4){
+                    var ages = [];
+                    for(var i=0; i<mods.length; i++){
+                        if(!waitingFor[mods[i]].ready){
+                            ages.push(mods[i]);
+                        }
+                    }
+                    log(2, `Modules are taking a long time to ready up: ${ages.join(", ")}`);
+                }
+                waited++;
+            }, 1000)
         }
     }
     return api;
