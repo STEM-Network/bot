f046112e6acae024f4984ed4ec861467d563ff9a
Azurethi <Github@Azurethi.me>
2020-06-25 17:54:28 +0100

    Added extra test cases all for new scoreadd command in example module

--- debugger.js
+++ debugger.js
@@ -2,12 +2,16 @@ var log = ()=>{};
 module.exports = (_log,cli)=>{
     log =_log;
     send('/score', cli); //Replace this with module added test cases
+    send('/scoreadd', cli);
+    send('/scoreadd 100 asdasd', cli);
+    send('/scoreadd 100', cli);
+    send('/score', cli);
 }
 
 //TODO implement more of the discord API
 function buildMessage(content){
     return {
-        user:{id:'test', tag:"test#1234"},
+        user:{id:'#test2', tag:"test#1234"},
         content,
         reply:(m)=>{log(4,`REPLY to "${content}": "${m}"`)}
     }
