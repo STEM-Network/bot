0124d5ecea97b17f1928b2351a24ec1827912e2f
Azurethi <Github@Azurethi.me>
2020-06-25 17:46:53 +0100

    Added module bind on logger to mgr setup

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ log('CORE', 3,"",{type:"startup"});
 
 var cli = new djs.Client();
 
-const mgr = require('./mgr')(cli,db,log,{regex:/^\/([a-zA-Z0-9_.]*)( |$)/});
+const mgr = require('./mgr')(cli,db,log.bind(null,"MGR"),{regex:/^\/([a-zA-Z0-9_.]*)( |$)/});
 
 var loadedModules = [];
 
@@ -19,18 +19,25 @@ fs.readdir('./modules',(err,modules)=>{
     modules.forEach(modName => {
         var mod = require(`./modules/${modName}`);
         loadedModules.push(mod.descriptor);
-        mod.init(log.bind(null,mod.descriptor.name),mgr,db,cli);
-        log('CORE', 3,"  ",{type:"moduleDesc",object:mod.descriptor});
+        try{
+            mgr.waitFor(mod.descriptor);
+            mod.init(log.bind(null,mod.descriptor.name),mgr,db,cli);
+            log('CORE', 3,"  ",{type:"moduleDesc",object:mod.descriptor});
+        } catch(e){
+            log('CORE', 3,"  ",{type:"moduleDescFail",object:mod.descriptor,reason:e});
+        }
+        
     });
-    
-    if(auth.token==""){
-        log('CORE', 2,"No Auth token, running in debug mode!");
-        //log('CORE', 3,"Severity formats:");
-        //for(var i=0; i<5; i++)log('CORE', i,`Severity ${i}`);
-        require('./debugger')(log.bind(null,'DEBUGGER'),cli);
-    } else {
-        cli.login(auth.token)
-    }
+    mgr.onReady(()=>{
+        if(auth.token==""){
+            log('CORE', 2,"No Auth token, running in debug mode!");
+            //log('CORE', 3,"Severity formats:");
+            //for(var i=0; i<5; i++)log('CORE', i,`Severity ${i}`);
+            require('./debugger')(log.bind(null,'DEBUGGER'),cli);
+        } else {
+            cli.login(auth.token)
+        }
+    })
 })
 
 //testing the github-discord hook
