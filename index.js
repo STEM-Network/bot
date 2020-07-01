92016271310b327406fab016fb72a141f6c0051d
Azurethi <Github@Azurethi.me>
2020-07-01 03:38:22 +0100

    Updated module loading logic - Now loads any commands in mod.cmds - Now loads any reqKeys in mod.requiredKeys - when live, now shows client tag

--- index.js
+++ index.js
@@ -17,9 +17,24 @@ log('CORE', 3,"Loading modules");
 fs.readdir('./modules',(err,modules)=>{
     if(err) throw err;
     modules.forEach(modName => {
+        if(modName.startsWith("_")) return; //ignore modules starting with _
         var mod = require(`./modules/${modName}`);
         loadedModules.push(mod.descriptor);
         try{
+            if(mod.cmds){
+                Object.keys(mod.requiredKeys).forEach(collection=>{
+                    mod.requiredKeys[collection].forEach(key=>{
+                        mgr.registerKey(collection,key,(err)=>{
+                            if(err) log(mod.descriptor.name,2,`Failed to create required key "${JSON.stringify(key)}" in ${collection}:: ${err}`);
+                        });
+                    });
+                });
+            }
+            if(mod.cmds){
+                Object.keys(mod.cmds).forEach(cmdName=>{
+                    mgr.registerCMD(cmdName,mod.cmds[cmdName].handler, mod.cmds[cmdName].options);
+                });
+            }
             mgr.waitFor(mod.descriptor);
             mod.init(log.bind(null,mod.descriptor.name),mgr,db,cli);
             log('CORE', 3,"  ",{type:"moduleDesc",object:mod.descriptor});
@@ -35,6 +50,9 @@ fs.readdir('./modules',(err,modules)=>{
             //for(var i=0; i<5; i++)log('CORE', i,`Severity ${i}`);
             require('./debugger')(log.bind(null,'DEBUGGER'),cli);
         } else {
+            cli.on('ready',()=>{
+                log('CORE', 3, `Live as ${cli.user.tag}`);
+            });
             cli.login(auth.token)
         }
     })
