9ab3cd2a0e7cf16458effeaca64f39b4ad37dfa6
Azurethi <Github@Azurethi.me>
2020-07-03 22:03:09 +0100

    Fixed a typo

--- modules/roles.js
+++ modules/roles.js
@@ -58,7 +58,7 @@ exports.cmds={
                             if(role){
                                 if(args[1]==".add"){
                                     for(var i=0; i<settings.allowedRoles.length; i++){
-                                        if(settings.allowedRoles[i].id === role.id)){
+                                        if(settings.allowedRoles[i].id === role.id){
                                             msg.channel.send("That role is already one of the allowed roles!");
                                             return;                            
                                         }
